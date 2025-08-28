b5537023935318b2644ec4455fb3c818ee303e90
yomnaahk <yomnakerir@gmail.com>
2025-08-29 01:23:36 +0300

    Day1

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Cocoa
+
+
+var str = "Hello, playground"
+str="Goodbay"
+
+var age = 18
+var population = 8_000_000
+
+var str1 = """
+This goes\
+over multiple\
+lines
+"""
+
+var pi = 3.14
+
+var awesome = true
+
+var score = 80
+var scoreString = "\(score) in the final"
+var result = "The test results is \(scoreString)"
+
+let taylor = "Swift"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
